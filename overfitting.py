7d0e638d7a2103223763145ee619f231b422dec4
jlybianto <jesse.lybianto@gmail.com>
2016-01-27 01:11:43 -0700

    Generate data and store into DataFrame

--- overfitting.py
+++ overfitting.py
@@ -10,3 +10,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import statsmodels.api as sm
+
+# ----------------
+# GENERATE DATA
+# ----------------
+
+# Set seed for reproducible results
+np.random.seed(414)
+
+# Generate data
+X = np.linspace(0, 15, 1000)
+y = 3 * np.sin(X) + np.random.normal(1 + X, 0.2, 1000)
+
+# Split into training set (70%) and test set (30%)
+train_X, train_y = X[:700], y[:700]
+test_X, test_y = X[700:], y[700:]
+
+train_df = pd.DataFrame({"X": train_X, "y": train_y})
+test_df = pd.DataFrame({"X": test_X, "y": test_y})
